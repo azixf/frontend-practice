fbd1095fa9cc6fa672f21dfe19a870045033c599
azixf <xief954@tydic.com>
2023-07-16 11:07:38 +0800

    feat:添加文字适配背景

--- src/pages/color-adapt-bg.vue
+++ src/pages/color-adapt-bg.vue
@@ -1,14 +1,30 @@
 <template>
-  <div class="page">
+  <div class="page place-center">
     <h1>文字适配背景</h1>
   </div>
 </template>
 <script lang="ts" setup>
-import { ref, reactive } from "vue";
-</script>
-<script lang="ts">
-export default {
+defineOptions({
   name: "文字适配背景",
-};
+});
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.page {
+  background-image: linear-gradient(
+    to right,
+    #000 0%,
+    #000 50%,
+    transparent 50%,
+    transparent 100%
+  );
+  h1 {
+    font-size: 30px;
+    color: #ffffff;
+    transition: transform 0.5s ease;
+    mix-blend-mode: difference;
+    &:hover {
+      transform: translate(-50px);
+    }
+  }
+}
+</style>
