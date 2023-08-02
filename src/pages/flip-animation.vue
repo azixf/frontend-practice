392485fb8c8556c8af9ac3b501f7ce5244608b9c
xiefei <xie0729@yeah.net>
2023-08-02 14:04:46 +0800

    feat:添加flip动画

--- src/pages/flip-animation.vue
+++ src/pages/flip-animation.vue
@@ -8,10 +8,11 @@
           <el-button type="info" @click="reset">重置</el-button>
         </el-space>
       </el-space>
-
-      <div class="img-box">
-        <img ref="img" src="/hand.png" alt="" @click="preview" />
-        <div ref="overlay" class="overlay" @click="hide"></div>
+      <img ref="img" class="img" src="/logo.svg" alt="" @click="preview" />
+      <div ref="overlay" class="overlay" @click="hide">
+        <div ref="imgBox" class="img-box">
+          <img ref="previewImg" src="" alt="" />
+        </div>
       </div>
     </el-space>
   </div>
@@ -63,7 +64,7 @@ function flipPlay() {
       },
     ],
     {
-      duration: 300,
+      duration: 500,
       easing: "cubic-bezier(0.68,-0.6,0.32,1.6)",
     }
   );
@@ -71,18 +72,55 @@ function flipPlay() {
 
 const img = shallowRef();
 const overlay = shallowRef();
+const imgBox = shallowRef();
+const previewImg = shallowRef();
 
-
-let start, end
+let start, end;
 
 function preview() {
-  img.value.style.position = "absolute";
-  img.value.style.top = "50%";
-  img.value.style.left = "50%";
-  img.value.style.transform = "translate3D(-50%, -50%, 0) scale(1.5)";
+  start = img.value.getBoundingClientRect();
+
+  previewImg.value.setAttribute("src", img.value.getAttribute("src"));
+  overlay.value.style.display = "grid";
+  img.value.style.opacity = 0;
+
+  end = imgBox.value.getBoundingClientRect();
+
+  previewAnimation(imgBox);
 }
 
-function hide() {}
+function previewAnimation(imgBox) {
+  const x = start.left - end.left;
+  const y = start.top - end.top;
+  const w = start.width / end.width;
+  const h = start.height / end.height;
+
+  imgBox.value.animate(
+    [
+      {
+        transform: `translate(${x}px, ${y}px) scale(${w})`,
+      },
+      {
+        transform: `translate(0, 0) scale(1)`,
+      },
+    ],
+    {
+      duration: 500,
+      easing: "cubic-bezier(0.2,0,0.2,1)",
+    }
+  );
+}
+
+function hide() {
+  start = imgBox.value.getBoundingClientRect();
+
+  overlay.value.style.display = "none";
+  img.value.style.opacity = 1;
+
+  end = img.value.getBoundingClientRect();
+
+  previewAnimation(img);
+}
 </script>
 
 <style lang="scss" scoped>
@@ -96,26 +134,37 @@ function hide() {}
   background-color: hsl(19, 87%, 48%);
 }
 
-.img-box {
-  width: 150px;
-  height: 120px;
-  border-radius: 20px;
-  border: 1px solid hsl(331, 87%, 48%);
-  img {
-    cursor: pointer;
-    width: 100%;
-    height: 100%;
-    object-fit: fill;
-  }
+.img {
+  width: 200px;
+  height: 200px;
+  cursor: pointer;
+}
 
-  .overlay {
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100vw;
-    height: 100vh;
-    background: rgba(0, 0, 0, 0.1);
-    display: none;
+.overlay {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.6);
+  display: none;
+  place-content: center;
+  transition: opacity 0.36s ease-in-out;
+  z-index: 1000;
+  .img-box {
+    width: 50vw;
+    height: 70vh;
+    transform-origin: top left;
+    // display: flex;
+    // flex-direction: column;
+    // margin: 100px auto;
+    img {
+      width: 100%;
+      height: 100%;
+      object-fit: contain;
+    }
   }
 }
 </style>
